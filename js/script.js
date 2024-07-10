01f98b907f000fc1d1503c673fc5064dd2df1b50
rgraper <robson.graper@gmail.com>
2024-07-10 10:31:18 -0300

    adicionei imagem e mudei pastas

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function chaves(texto) {
+    let caracteres = texto.value.split("");
+
+    if (item == "e") {
+        caracteres[i] = "enter";   
+
+    }   else if (item == "i") {
+        caracteres[i] = "imes"
+        
+    }   else if (item == "a") {
+        caracteres[i] = "ai"
+        
+    }   else if (item == "o") {
+        caracteres[i] = "ober"
+        
+    }   else if (item == "u") {
+        caracteres[i] = "ufat"
+        
+    }
+
+    return caracteres.join("");
+}
+
+function criptografar() {
+    console.log("TÁ CLICANDO!")
+
+}
+
