f22f920d843dddd6a216fbff04efdbfa76f7e6e9
rgraper <robson.graper@gmail.com>
2024-07-10 13:30:12 -0300

    apliquei botao copiar

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-function chaves(texto) {
-    let caracteres = texto.value.split("");
-
-    if (item == "e") {
-        caracteres[i] = "enter";   
-
-    }   else if (item == "i") {
-        caracteres[i] = "imes"
-        
-    }   else if (item == "a") {
-        caracteres[i] = "ai"
-        
-    }   else if (item == "o") {
-        caracteres[i] = "ober"
-        
-    }   else if (item == "u") {
-        caracteres[i] = "ufat"
-        
-    }
-
-    return caracteres.join("");
-}
-
 function criptografar() {
-    console.log("TÁ CLICANDO!")
+    let criptografa = document.querySelector(".input__texto").value;
 
+    let resultadocript = criptografa
+        .split('e').join('enter')
+        .split('i').join('imes')
+        .split('a').join('ai')
+        .split('o').join('ober')
+        .split('u').join('ufat');
+
+    document.querySelector(".texto__transformado").value = resultadocript;
 }
 
+function descriptografar() {
+    let descriptografa = document.querySelector(".input__texto").value;
+
+    let resultadodescript = descriptografa
+        .split('enter').join('e')
+        .split('imes').join('i')
+        .split('ai').join('a')
+        .split('ober').join('o')
+        .split('ufat').join('u');
+
+    document.querySelector(".texto__transformado").value = resultadodescript;
+}
+
+function copiar() {
+    let copiaTexto = document.querySelector(".texto__transformado");
+    copiaTexto.select();
+    document.execCommand("copy");
+  }
+  
+  document.querySelector("#copy").addEventListener("click", copy);
+  
